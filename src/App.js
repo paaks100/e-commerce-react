04f837ec421f936649b92819daf51e02b7479071
paaks100 <pkkodua@gmail.com>
2023-04-05 14:26:01 +0000

    commit pre-comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import './App.css';
 import { Navbar } from './components/navbar';
 import { Cart } from './pages/cart/cart';
 import { Shop } from './pages/shop/shop';
+import { ShopContextProvider } from './context/shop-context';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Shop/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Shop/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext } from '../../context/shop-context';
 
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
-  return (
-    <div>{productName}</div>
-  )
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemAmount = cartItems[id];
+    return (
+        <div className='product'>
+            <img src={productImage}/>
+            <div className='description'>
+                <p> <b>{productName}</b> </p>
+                <p> ${price} </p>
+            </div>
+            <button className='addToCartBttn' onClick={() => addToCart(id)}> Add To Cart {cartItemAmount > 0 && <>{cartItemAmount}</>} </button>
+        </div>
+    )
 }
